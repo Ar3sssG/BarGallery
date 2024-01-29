f154b8eb6035c3db87537e99da2f52b76ee1ed97
Vahe Grigoryan <ar3sssg@gmail.com>
2024-01-29 12:24:45 +0400

    deleted

--- BarGalleryApp/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import router from './route';
-
-const App = () => {
-    return <RouterProvider router={router} />
-}
-
-export default App;
