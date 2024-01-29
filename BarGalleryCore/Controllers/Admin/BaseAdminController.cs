c23304ee98b21dc6805a471d410f57b6070575b2
Vahe Grigoryan <ar3sssg@gmail.com>
2024-01-29 12:18:25 +0400

    added admin controller

---
+++ BarGalleryCore/Controllers/Admin/BaseAdminController.cs
@@ -0,0 +1,25 @@
+﻿using AdaptiveStoreAdmin.Controllers;
+using BGDataLayer.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarGalleryCore.Controllers.Admin
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [Authorize(AuthenticationSchemes = "Bearer",Roles = "ADMIN")]
+    public class BaseAdminController : ControllerBase
+    {
+        protected readonly ILogger<BaseController> _logger;
+        protected UserManager<User> _userManager;
+        protected SignInManager<User> _signInManager;
+        public BaseAdminController(UserManager<User> userManager, SignInManager<User> signInManager, ILogger<BaseController> logger = null)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _logger = logger;
+        }
+    }
+}
